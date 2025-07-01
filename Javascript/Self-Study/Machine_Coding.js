2c53fa3ee81d010d8d035d1ee359d05e1017801d
StarFire1701 <projectscollege.1701@gmail.com>
2025-07-01 15:15:56 +0000

    Machine Coding Day 1

--- Javascript/Self-Study/Machine_Coding.js
+++ Javascript/Self-Study/Machine_Coding.js
@@ -9,10 +9,29 @@ Array.prototype.myforEach=function(userfn){
     }
 }
 }
+
+if(!Array.prototype.mymap){
+    Array.prototype.mymap = function(userfn){
+        const res=[]
+        for(let i=0;i<this.length;i++){
+            const temp=userfn(this[i],i)
+            res.push(temp)
+        }
+        return res
+    }
+}
 const arr=[1,2,3];
-arr.myforEach((value,index) => {
-        console.log(`For my for each ${value} is the value at ${index}`)
-});
+// arr.myforEach((value,index) => {
+//         console.log(`For my for each ${value} is the value at ${index}`)
+// });
+
+
+const arr2 = arr.mymap((e)=>{
+    e*3
+})
+
+console.log(arr2)
+
 
 
 
