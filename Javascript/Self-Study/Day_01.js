542b7312da2ad1fd992076edbaf469a3e67db6dc
StarFire1701 <projectscollege.1701@gmail.com>
2025-07-17 17:34:23 +0000

    Self Practice Day 1

---
+++ Javascript/Self-Study/Day_01.js
@@ -0,0 +1,20 @@
+// Problem: Create an Array Containing Different Types of Teas
+teas=["Lemon Tea","Oolong Tea","Herbal Tea",""]
+
+// Problem: Add "Chamomile Tea" to the existing list of teas
+
+// Problem: Remove "Oolong Tea" from the list of teas
+
+// Problem: Filter the list to only include teas that are caffeinated
+
+// Problem: Sort the list of teas in alphabetical order
+
+// Problem: Use a for loop to print each type of teas in array
+
+// Problem: Use a for loop to calculate how many teas are caffeinated (excluding "Herbal Tea")
+
+// Problem: Use a for loop to create a new array with all tea names in uppercase
+
+// Problem: Use a for loop to find the tea name with most characters.
+
+// Problem: Use a for loop to reverse the order of teas to the array. 
