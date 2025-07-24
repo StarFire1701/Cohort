735ab90de0d17264a73ca3252084802749263058
StarFire1701 <projectscollege.1701@gmail.com>
2025-07-24 06:43:33 +0000

    Arrays Revision

--- Javascript/Arrays/Arrays_01.js
+++ Javascript/Arrays/Arrays_01.js
@@ -27,6 +27,23 @@ const nums=[1,2,3,4,5,6,7]
 // })
 
 // Using call backs
-nums.forEach((value,index)=>{
-    console.log(`Value at ${index} is ${value}`)
-})
+// nums.forEach((value,index)=>{
+//     console.log(`Value at ${index} is ${value}`)
+// })
+
+// let arr=[1,2,2,4,6,6,6]
+
+// let arr=[1,2,2,4,6,6,6]
+// let a=21
+// console.log(Array.isArray(arr))
+// console.log(Array.isArray(a))
+let arr=[1,2,3,4,5,6,7,8,9]
+//     =[0,1,2,3,4,5,6,7,8]
+//     =[1,2,4,5,]
+
+// const checkfunc=(currentvalue)=>{currentvalue>5}
+arr.copyWithin(2,3,5)
+console.log(arr)
+console.log(arr.every(function(currentvalue){
+    return currentvalue>5
+}))
